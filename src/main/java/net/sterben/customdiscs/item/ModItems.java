aa0b3a5498b5317309c9938b90ac224eb744ee4d
STERBEN <sarashinasterben@gmail.com>
2025-03-27 00:38:24 +0800

    added a bunch of new songs and updated to 1.21.4

--- src/main/java/net/sterben/customdiscs/item/ModItems.java
+++ src/main/java/net/sterben/customdiscs/item/ModItems.java
@@ -19,8 +19,37 @@ import java.util.function.Function;
 
 public class ModItems {
     // Add Items Here
+    // One Click Straight Songs
     public static final Item TELEPONO_MUSIC_DISC = registerItem("telepono_music_disc",
             Item::new, new Item.Settings().jukeboxPlayable(ModSounds.TELEPONO_KEY).maxCount(1));
+
+    public static final Item MRT_MUSIC_DISC = registerItem("mrt_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.MRT_KEY).maxCount(1));
+
+    public static final Item DAHAN_DAHAN_MUSIC_DISC = registerItem("dahan_dahan_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.DAHAN_DAHAN_KEY).maxCount(1));
+
+    public static final Item SIGA_MUSIC_DISC = registerItem("siga_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.SIGA_KEY).maxCount(1));
+
+    // SUYEN Songs
+    public static final Item BENTE_MUSIC_DISC = registerItem("bente_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.BENTE_KEY).maxCount(1));
+
+    public static final Item SUYEN_IMNIDA_MUSIC_DISC = registerItem("suyen_imnida_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.SUYEN_IMNIDA_KEY).maxCount(1));
+
+    public static final Item ISANG_LINGGO_MUSIC_DISC = registerItem("isang_linggo_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.ISANG_LINGGO_KEY).maxCount(1));
+
+    public static final Item SOMETHING_ABOUT_YOU_MUSIC_DISC = registerItem("something_about_you_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.SOMETHING_ABOUT_YOU_KEY).maxCount(1));
+
+    public static final Item TAMPO_MUSIC_DISC = registerItem("tampo_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.TAMPO_KEY).maxCount(1));
+
+    public static final Item TEKA_LANG_MUSIC_DISC = registerItem("teka_lang_music_disc",
+            Item::new, new Item.Settings().jukeboxPlayable(ModSounds.TEKA_LANG_KEY).maxCount(1));
 
     // Helper
     public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
@@ -40,7 +69,18 @@ public class ModItems {
     public static void registerModItems() {
         Registry.register(Registries.ITEM_GROUP, CUSTOM_DISCS_GROUP_KEY, CUSTOM_DISCS_GROUP);
             ItemGroupEvents.modifyEntriesEvent(CUSTOM_DISCS_GROUP_KEY).register(itemGroup -> {
+                // OCS
+                itemGroup.add(MRT_MUSIC_DISC);
+                itemGroup.add(DAHAN_DAHAN_MUSIC_DISC);
                 itemGroup.add(TELEPONO_MUSIC_DISC);
+                itemGroup.add(SIGA_MUSIC_DISC);
+                // SUYEN
+                itemGroup.add(BENTE_MUSIC_DISC);
+                itemGroup.add(SUYEN_IMNIDA_MUSIC_DISC);
+                itemGroup.add(ISANG_LINGGO_MUSIC_DISC);
+                itemGroup.add(SOMETHING_ABOUT_YOU_MUSIC_DISC);
+                itemGroup.add(TAMPO_MUSIC_DISC);
+                itemGroup.add(TEKA_LANG_MUSIC_DISC);
         });
     }
 }
--- src/main/java/net/sterben/customdiscs/sound/ModSounds.java
+++ src/main/java/net/sterben/customdiscs/sound/ModSounds.java
@@ -11,9 +11,47 @@ import net.sterben.customdiscs.CustomDiscs1214;
 
 public class ModSounds {
     // Add Songs Here
+    // One Click Straight Songs
     public static final SoundEvent TELEPONO = registerSoundEvent("telepono");
     public static final RegistryKey<JukeboxSong> TELEPONO_KEY =
             RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "telepono"));
+
+    public static final SoundEvent MRT = registerSoundEvent("mrt");
+    public static final RegistryKey<JukeboxSong> MRT_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "mrt"));
+
+    public static final SoundEvent DAHAN_DAHAN = registerSoundEvent("dahan_dahan");
+    public static final RegistryKey<JukeboxSong> DAHAN_DAHAN_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "dahan_dahan"));
+
+    public static final SoundEvent SIGA = registerSoundEvent("siga");
+    public static final RegistryKey<JukeboxSong> SIGA_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "siga"));
+
+    // SUYEN Songs
+    public static final SoundEvent BENTE = registerSoundEvent("bente");
+    public static final RegistryKey<JukeboxSong> BENTE_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "bente"));
+
+    public static final SoundEvent SUYEN_IMNIDA = registerSoundEvent("suyen_imnida");
+    public static final RegistryKey<JukeboxSong> SUYEN_IMNIDA_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "suyen_imnida"));
+
+    public static final SoundEvent ISANG_LINGGO = registerSoundEvent("isang_linggo");
+    public static final RegistryKey<JukeboxSong> ISANG_LINGGO_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "isang_linggo"));
+
+    public static final SoundEvent SOMETHING_ABOUT_YOU = registerSoundEvent("something_about_you");
+    public static final RegistryKey<JukeboxSong> SOMETHING_ABOUT_YOU_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "something_about_you"));
+
+    public static final SoundEvent TAMPO = registerSoundEvent("tampo");
+    public static final RegistryKey<JukeboxSong> TAMPO_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "tampo"));
+
+    public static final SoundEvent TEKA_LANG = registerSoundEvent("teka_lang");
+    public static final RegistryKey<JukeboxSong> TEKA_LANG_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(CustomDiscs1214.MOD_ID, "teka_lang"));
 
     // Helper
     private static SoundEvent registerSoundEvent(String name) {
